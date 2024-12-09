7b075fcbee72081fb54f7ed4e44e13fa738762a8
Wesley <wesleysant909@gmail.com>
2024-12-09 09:34:58 -0300

    upup

--- desafios/Treino 3, site de carros esportivos/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const detailsButton = document.getElementById('detailsButton');
-    if (detailsButton) {
-        detailsButton.addEventListener('click', () => {
-            window.location.href = 'motos.html';
-        });
-    }
-});
