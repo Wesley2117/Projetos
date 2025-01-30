21d6ec608b75b4257626a9d9312215705bda0105
Wesley2117 <wesleysant741@gmail.com>
2025-01-29 22:02:38 -0300

    upup

---
+++ food/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile_btn').addEventListener('click', function() {
+    const mobileMenu = document.getElementById('mobile_menu');
+    mobileMenu.style.display = mobileMenu.style.display === 'block' ? 'none' : 'block';
+});
